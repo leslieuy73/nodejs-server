95ed5211b3b05f16168521ccbce343181ee5f6b2
Eden Ramoneda <edenlee143@gmail.com>
2023-10-08 01:35:27 +0800

    added routes for getting categories and subitems

--- server.js
+++ server.js
@@ -52,6 +52,92 @@ app.get('/api/datasheet/:id', (req, res) => {
   }
 });
 
+app.get('/api/categories/:id', (req, res) => {
+  try {
+    // Create a new Curl instance for each request
+    const curlTest = new Curl();
+
+    // Configure the Curl request
+    curlTest.setOpt(Curl.option.URL, `${apiUrl}category/viewCatergories?datasheetid=${req.params.id}`); // Replace with your API URL
+    //curlTest.setOpt(Curl.option.HTTPHEADER, ['Content-Type: application/json']);
+
+    // Set up response handling
+    curlTest.on('end', function (statusCode, data, headers) {
+      // console.info('Response status code: ' + statusCode);
+      // console.info('Response data: ' + data);
+    
+      if (statusCode === 200) {
+        // Send the API response to the client
+        res.status(200).json(JSON.parse(data));
+      } else {
+        // Handle other status codes as needed
+        res.status(statusCode).json({ error: 'API request failed' });
+      }
+
+      // Close the Curl instance
+      curlTest.close();
+    });
+
+    // Handle Curl request errors
+    curlTest.on('error', function (error) {
+      console.error('Curl error: ' + error);
+      res.status(500).json({ error: 'An error occurred while accessing the API.' });
+
+      // Close the Curl instance
+      curlTest.close();
+    });
+
+    // Perform the Curl request
+    curlTest.perform();
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'An error occurred while accessing the API.' });
+  }
+});
+
+app.get('/api/categories/subItems/:catId', (req, res) => {
+  try {
+    // Create a new Curl instance for each request
+    const curlTest = new Curl();
+
+    // Configure the Curl request
+    curlTest.setOpt(Curl.option.URL, `${apiUrl}item/viewItems?categoryID=${req.params.catId}`); // Replace with your API URL
+    //curlTest.setOpt(Curl.option.HTTPHEADER, ['Content-Type: application/json']);
+
+    // Set up response handling
+    curlTest.on('end', function (statusCode, data, headers) {
+      // console.info('Response status code: ' + statusCode);
+      // console.info('Response data: ' + data);
+    
+      if (statusCode === 200) {
+        // Send the API response to the client
+        res.status(200).json(JSON.parse(data));
+      } else {
+        // Handle other status codes as needed
+        res.status(statusCode).json({ error: 'API request failed' });
+      }
+
+      // Close the Curl instance
+      curlTest.close();
+    });
+
+    // Handle Curl request errors
+    curlTest.on('error', function (error) {
+      console.error('Curl error: ' + error);
+      res.status(500).json({ error: 'An error occurred while accessing the API.' });
+
+      // Close the Curl instance
+      curlTest.close();
+    });
+
+    // Perform the Curl request
+    curlTest.perform();
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'An error occurred while accessing the API.' });
+  }
+});
+
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
